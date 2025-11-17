4fadce407866ebcebe786d62d43c781520269b11
ViktoriaZaharova <zaharova181114@gmail.com>
2025-11-18 00:57:38 +0200

    = 1 section

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
+
 $('.home-slider').slick({
     slidesToShow: 1,
     fade: true,
@@ -8,4 +12,40 @@ $('.home-slider').slick({
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev btn-pink"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next btn-pink"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
+$('.popular-category-slider').slick({
+    slidesToShow: 5,
+    arrows: true,
+    dots: true,
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-prev btn-accent"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next btn-accent"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }
+  ]
 });
