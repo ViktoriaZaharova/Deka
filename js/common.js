a312aeb9b98c1bf10e7526b24e9f85cfbd97e1f4
ViktoriaZaharova <zaharova181114@gmail.com>
2025-11-20 13:28:21 +0200

    1

--- js/common.js
+++ js/common.js
@@ -56,7 +56,52 @@ $('.reviews-slider').slick({
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev btn-pink slick-arrow-v2"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next btn-pink slick-arrow-v2"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }
+  ]
 });
+
+$('.hits-slider').slick({
+    slidesToShow: 4,
+    arrows: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev btn-pink slick-arrow-v2"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next btn-pink slick-arrow-v2"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        arrows: false
+      }
+    }
+  ]
+});
+
 
 // reviews loader text
 $(document).ready(function () {
