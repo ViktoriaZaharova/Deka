e4ae935b8651508e0b8f60f5cb4b6ff4e19260bf
Viktoria Zaharova <zaharova181114@gmail.com>
2025-11-30 01:04:09 +0200

    news page

--- js/common.js
+++ js/common.js
@@ -532,3 +532,12 @@ $(function () {
   });
 
 });
+
+
+// tabs mobile
+$(function () {
+  $('.tabs-select-btn').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent().toggleClass('open');
+  });
+});
